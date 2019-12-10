65eb5b9a431b22be3db5cacb51f2624233096323
Yue Sun <ysun276@wisc.edu>
2019-12-10 13:17:32 -0500

    result output system

--- evaluate_poems.py
+++ evaluate_poems.py
@@ -5,7 +5,6 @@ from generateModel import GenerateModel
 from paths import data_sxhy_path
 
 import jieba
-import csv
 import json
 
 """
@@ -24,57 +23,42 @@ def eval_poems(poem, sorted_poem_scores1, sorted_poem_scores2, sorted_poem_score
     # process first lien
     target_score = get_score(poem[0])
     target_poem = binarySearch(sorted_poem_scores1, target_score, 1)
-    rv_score += abs(target_poem.score2 - target_score)
+    rv_score += abs(target_poem['score2'] - target_score)
 
     # process 2 lien
     target_score = get_score(poem[1])
     target_poem = binarySearch(sorted_poem_scores2, target_score, 2)
-    rv_score += abs(target_poem.score3 - target_score)
+    rv_score += abs(target_poem['score3'] - target_score)
 
     target_score = get_score(poem[2])
     target_poem = binarySearch(sorted_poem_scores3, target_score, 3)
-    rv_score += abs(target_poem.score4 - target_score)
+    rv_score += abs(target_poem['score4'] - target_score)
 
     return rv_score
 
 
 def binarySearch(sorted_poem_scores, target_score, num_of_sentence):
     def get_sentence_score(poem_score, num_of_sentence):
-
         if num_of_sentence == 1:
-
-            return poem_score.score1
-
+            return poem_score["score1"]
         elif num_of_sentence == 2:
-
-            return poem_score.score2
-
+            return poem_score["score2"]
         elif num_of_sentence == 3:
-
-            return poem_score.score3
-
+            return poem_score["score3"]
         else:
-
-            return poem_score.score4
+            return poem_score["score4"]
 
     l = 0
     r = len(sorted_poem_scores) - 1
 
     while (l + 1 < r):
-
         mid = (l + r) // 2
 
         if (get_sentence_score(sorted_poem_scores[mid], num_of_sentence) < target_score):
-
             l = mid
-
-
         elif (get_sentence_score(sorted_poem_scores[mid], num_of_sentence) > target_score):
-
             r = mid
-
         else:
-
             return sorted_poem_scores[mid]
 
     if (abs(get_sentence_score(sorted_poem_scores[l], num_of_sentence) - target_score) > abs(
@@ -149,36 +133,41 @@ def get_score(sentence):
 def main():
     planner = Planner()
     generator = GenerateModel(False)
-    print("Start to preprocess score")
-    poem_scores = get_poem_scores()
-    with open("poem_scores.txt", "w") as f:
-        json_data = json.dumps([score.__dict__ for score in poem_scores])
-        json.dump(json_data, f)
-    print("score calculation completed")
-    '''
+    # print("Start to preprocess score")
+    # poem_scores = get_poem_scores()
+    # with open("poem_scores.txt", "w") as f:
+    #     json_data = json.dumps([score.__dict__ for score in poem_scores])
+    #     json.dump(json_data, f)
+    # print("score calculation completed")
+
+    with open("poem_scores.txt", 'r') as f:
+        poem_scores = json.loads(json.load(f))
+
     print("process score1...")
-    sorted_poem_scores1 = sorted(poem_scores, key=lambda curr_poem: curr_poem.score1)
+    sorted_poem_scores1 = sorted(poem_scores, key=lambda curr_poem: curr_poem['score1'])
     print("process score2...")
-    sorted_poem_scores2 = sorted(poem_scores, key=lambda curr_poem: curr_poem.score2)
+    sorted_poem_scores2 = sorted(poem_scores, key=lambda curr_poem: curr_poem['score2'])
     print("process score3...")
-    sorted_poem_scores3 = sorted(poem_scores, key=lambda curr_poem: curr_poem.score3)
+    sorted_poem_scores3 = sorted(poem_scores, key=lambda curr_poem: curr_poem['score3'])
 
     avg_score = 0
     num = 500
 
-    for line in open(data_sxhy_path):
-        listWords = line.split()
-    for i in range(num):
-        keyword = listWords[i]
-        keywords = planner.plan(keyword)
-        poem = generator.generate(keywords)
-        score = eval_poems(poem, sorted_poem_scores1, sorted_poem_scores2, sorted_poem_scores3)
-        avg_score += score
-        for sentence in poem:
-            print(sentence)
-        print("The score of the current poem is:" + str(score))
-    print("The average score is:" + str(avg_score / num))
-    '''
+    with open("scores_result.txt", "w") as f:
+        for line in open(data_sxhy_path):
+            listWords = line.split()
+        for i in range(num):
+            keyword = listWords[i]
+            keywords = planner.plan(keyword)
+            poem = generator.generate(keywords)
+            score = eval_poems(poem, sorted_poem_scores1, sorted_poem_scores2, sorted_poem_scores3)
+            avg_score += score
+            for sentence in poem:
+                f.write(sentence)
+                f.write("\n")
+            f.write("The score of the current poem is:" + str(score))
+            f.write("\n")
+        f.write("The average score is:" + str(avg_score / num))
 
 if __name__ == '__main__':
     main()
